48218f75ae56a0a4be4d055bc84c20a4a8ffcae9
Ivan Bravo <enigmavoodoo@gmail.com>
2016-04-27 12:50:22 +0200

    Added Suggestion class

---
+++ app/src/main/java/model/Suggestion.java
@@ -0,0 +1,22 @@
+package model;
+
+/***
+ * Created by Ivan on 27/04/2016.
+ */
+
+public class Suggestion {
+
+    private String name;
+
+    public Suggestion(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
