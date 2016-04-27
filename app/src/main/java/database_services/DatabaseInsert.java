10652752530bd66f52a21dce7f08d64b7b1e9ad5
Ivan Bravo <enigmavoodoo@gmail.com>
2016-04-27 17:14:02 +0200

    First part added database services

---
+++ app/src/main/java/database_services/DatabaseInsert.java
@@ -0,0 +1,33 @@
+package database_services;
+
+import android.content.Context;
+
+import io.realm.Realm;
+import model.Suggestion;
+
+/***
+ * Created by Ivan on 27/04/2016.
+ */
+
+public class DatabaseInsert {
+
+    private Realm realm;
+    private Context context;
+
+    public DatabaseInsert(Context context){
+        this.context = context;
+        realm = Realm.getInstance(context);
+    }
+
+    public void insertNewSuggestion(Suggestion suggestion){
+        int newID = realm.allObjects(Suggestion.class).size() + 1;
+        // All changes to data must happen in a transaction
+        realm.beginTransaction();
+
+
+
+
+
+
+    }
+}
