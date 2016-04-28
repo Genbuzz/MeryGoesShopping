a58d83aa51a8bccc152b27be5e58c9666d1a9f3d
Ivan Bravo <enigmavoodoo@gmail.com>
2016-04-28 12:49:26 +0200

    Testing Realm database

--- app/src/main/java/database_services/DatabaseInsert.java
+++ app/src/main/java/database_services/DatabaseInsert.java
@@ -1,9 +1,6 @@
 package database_services;
 
-import android.content.Context;
 
-import io.realm.Realm;
-import model.Suggestion;
 
 /***
  * Created by Ivan on 27/04/2016.
@@ -11,13 +8,7 @@ import model.Suggestion;
 
 public class DatabaseInsert {
 
-    private Realm realm;
-    private Context context;
 
-    public DatabaseInsert(Context context){
-        this.context = context;
-        realm = Realm.getInstance(context);
-    }
 
 
 }
--- app/src/main/java/model/Suggestion.java
+++ app/src/main/java/model/Suggestion.java
@@ -2,18 +2,20 @@ package model;
 
 import io.realm.RealmObject;
 import io.realm.annotations.Ignore;
+import io.realm.annotations.PrimaryKey;
 
 /***
  * Created by Ivan on 27/04/2016.
  */
 
-public class Suggestion extends RealmObject {
+public class Suggestion extends RealmObject{
 
     @Ignore
     public static final String SUGGESTION_ID = "suggestion_id";
     @Ignore
     public static final String SUGGESTION_NAME = "suggestion_name";
 
+    @PrimaryKey
     private int id;
     private String name;
 
