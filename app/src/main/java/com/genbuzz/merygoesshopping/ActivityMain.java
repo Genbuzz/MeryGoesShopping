34913261900910cb2a56b041a45d673c73bc3247
Ivan Bravo <enigmavoodoo@gmail.com>
2016-05-09 21:52:41 +0200

    ActivityMain implementation

--- app/src/main/java/com/genbuzz/merygoesshopping/ActivityMain.java
+++ app/src/main/java/com/genbuzz/merygoesshopping/ActivityMain.java
@@ -1,27 +1,40 @@
 package com.genbuzz.merygoesshopping;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-/**
- * keep models and presenters android free
-   let the views handle everything that touches UI thread
- */
-public class ActivityMain extends AppCompatActivity {
+import android.view.View;
+import android.widget.ImageView;
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import presenter.MainPresenter;
+import view.MainView;
 
 
+public class ActivityMain extends AppCompatActivity implements MainView {
+
+    private MainPresenter mMainPresenter;
+    @BindView(R.id.activity_main_icon_barcode) ImageView mBarIcon;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-
-
+        setContentView();
+        ButterKnife.bind(this);
+        mMainPresenter = new MainPresenter(this);
+        mMainPresenter.onCreate();
     }
 
+    @Override
+    public void setContentView() {
+        setContentView(R.layout.activity_main);
+    }
 
-
-
+    @Override
+    public void launchActivityDetail() {
+        Intent intent = new Intent(this, ActivityDetail.class);
+        startActivity(intent);
+    }
 
 
 }
